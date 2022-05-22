39e3b482f658574ac39703862327a375745b6a3e
Abdelkader <Abelgheddouche@gmail.com>
2022-05-22 23:37:50 +0200

    b

--- public/electron.js
+++ public/electron.js
@@ -16,8 +16,8 @@ const PDFWindow = require("electron-pdf-window");
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
