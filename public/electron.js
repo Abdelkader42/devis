bf0cbbd6b2e9e4f21fdeec713a5f520575db1ee1
Abdelkader <Abelgheddouche@gmail.com>
2022-05-22 18:56:26 +0200

    add updater

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { autoUpdater } = require("electron-updater");
 
 const {
   app,
@@ -30,6 +31,10 @@ function createWindow() {
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
+
+  if (!isDev) {
+		autoUpdater.checkForUpdates();
+	};
   // Open the DevTools.
   // if (isDev) {
   //   win.webContents.openDevTools({ mode: "detach" });
@@ -92,3 +97,29 @@ function renderPdf(url) {
 //    win.loadURL(url);
  // renderToFile(url,'./my-doc.pdf')
 }
+
+autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
+	const dialogOpts = {
+		type: 'info',
+		buttons: ['Ok'],
+		title: 'Application Update',
+		message: process.platform === 'win32' ? releaseNotes : releaseName,
+		detail: 'A new version is being downloaded.'
+	}
+	dialog.showMessageBox(dialogOpts, (response) => {
+
+	});
+})
+
+autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
+	const dialogOpts = {
+		type: 'info',
+		buttons: ['Restart', 'Later'],
+		title: 'Application Update',
+		message: process.platform === 'win32' ? releaseNotes : releaseName,
+		detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+	};
+	dialog.showMessageBox(dialogOpts).then((returnValue) => {
+		if (returnValue.response === 0) autoUpdater.quitAndInstall()
+	})
+});
